a1eec31b146aa03de94a2b350529d2100ea8922c
Karl Brodowsky <bk1@gmx.net>
2017-03-19 23:23:58 +0100

    HeapSort made generic

--- sort/HeapSort.scala
+++ sort/HeapSort.scala
@@ -8,50 +8,63 @@
  * leave the heap in reverse sorted order, so as the heap shrinks, we can put popped
  * values to the right end of the same array.
  */
+
+import scala.util.Random
+
 object HeapSort {
   def main(args: Array[String]): Unit = {
-    val mess = Array(3, 9, 8, 13, 2, 5, 4);
 
-    val hs : HeapSort[Int] = new HeapSort();
-    hs.sort(mess)
-   // buildHeap(mess, mess.length-1)
+    val hs1 : HeapSort[Int] = new HeapSort();
+    val mess0 : Array[Int] = Array.fill(10)(Math.abs(Random.nextInt));
+    hs1.sort(mess0)
+    mess0.foreach( println )
+    println
 
-    mess.foreach( println )
+    val mess1 = Array(3, 9, 8, 13, 2, 5, 4);
+    hs1.sort(mess1)
+    mess1.foreach( println )
+    println
+
+    val hs2 : HeapSort[String] = new HeapSort();
+    val mess2 = Array("3", "x9", "y8", "и13", "ü2", "ö5", "ярпыж4");
+    hs2.sort(mess2)
+    mess2.foreach( println )
+    println
   }
 }
 
-class HeapSort[T] {
+class HeapSort[T : Ordering] {
 
-  def sort(a: Array[Int]): Unit = {
+  def sort(a: Array[T])(implicit ordering : Ordering[T]): Unit = {
     var m = a.length - 1 
-    buildHeap(a, m)
+    buildHeap(a, m)(ordering)
     while (m >= 1) {
       swap(a, 0, m)
       m-=1
-      heapify(a, 0, m)
+      heapify(a, 0, m)(ordering)
     }
   }
 
-  private def buildHeap(a: Array[Int], m: Int): Unit = {
+  private def buildHeap(a: Array[T], m: Int)(implicit ordering : Ordering[T]): Unit = {
     for (i <- m/2 to 0 by -1) {
-      heapify(a, i, m)
+      heapify(a, i, m)(ordering)
     }
   }
 
   /**Pushes an illegally located element down the heap to restore heap property.*/
   @annotation.tailrec
-  private def heapify(a: Array[Int], loc: Int, lastLeaf: Int): Unit = {
+  private def heapify(a: Array[T], loc: Int, lastLeaf: Int)(implicit ordering : Ordering[T]) : Unit = {
     val l = left(loc) 
     val r = right(loc)
 
     var max = loc
 
-    if(l <= lastLeaf && a(l) > a(max)) max = l
-    if(r <= lastLeaf && a(r) > a(max)) max = r
+    if (l <= lastLeaf && ordering.compare(a(l), a(max)) > 0) { max = l }
+    if (r <= lastLeaf && ordering.compare(a(r), a(max)) > 0) { max = r }
 
-    if(max != loc) {
+    if (max != loc) {
       swap(a, max, loc)
-      heapify(a, max, lastLeaf)
+      heapify(a, max, lastLeaf)(ordering)
     }
   }
 
@@ -65,7 +78,7 @@ class HeapSort[T] {
     return 2*loc+1
   }
 
-  private def swap(a: Array[Int], i: Int, j:Int): Unit = {
+  private def swap(a: Array[T], i: Int, j:Int): Unit = {
     val staging = a(i)
     a(i) = a(j)
     a(j) = staging
